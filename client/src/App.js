33f3b64edda356ab1b7b12fd4ca6414dd0ed546f
Juan Montes <juanmontes@criptext.com>
2024-08-11 23:16:53 -0500

    ContactList with Styles added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+/* import InteractionPage from './pages/InteractionPage';
+import SalesPage from './pages/SalesPage';
+import ReportPage from './pages/ReportPage'; */
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/contacts" element={<ContactPage />} />
+        {/*        <Route path="/interactions" element={<InteractionPage />} />
+        <Route path="/sales" element={<SalesPage />} />
+        <Route path="/reports" element={<ReportPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
